bf92694b9045b4739f6463e9491abefa1a626367
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-02-16 12:07:33 -0600

    Eliminar codigo

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//import Cajas from './components/Cajas';
-//import ProporcionarOP from './components/ProporcionarOP';
-
-//import { Route, Router, Link, browserHistory, IndexRoute } from 'react-router';
-
 ReactDOM.render(
  <div className="container"><App/></div>
  ,
   document.getElementById('app')
 );
 
-/*
-
-
-
-	<Router history = {browserHistory} >
-		<Route path="/" component = {App}>
-			<IndexRoute component = {Cajas} />
-			<Route path = "ops" component = {ProporcionarOP} />
-		</Route>
-	</Router>
-<Route path = "home" component = {Home} />
-			<Route path = "about" component = {About} />
-			<Route path = "contact" component = {Contact} /
-
-*/
