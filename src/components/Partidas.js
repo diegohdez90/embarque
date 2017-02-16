ed1e3d777ad627041f81ae89c41a10bc9b683fe7
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-02-16 12:06:37 -0600

    Reparacion de bugs

--- src/components/Partidas.js
+++ src/components/Partidas.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Partidas extends Component{
 	constructor(props){	
 		super(props);
-		this.partidas = this.props.partidas[0];
+		this.partidas = this.props.partidas;
 	}
 
 	addRelation(v){
@@ -13,16 +13,15 @@ class Partidas extends Component{
 	addPartidas(){
 		var partidas_rows =  [];
 		for ( var i in this.partidas ) {
-			partidas_rows.push(<tr key={i}><td colSpan="2">{i} { (this.partidas[i].length>0) ? '' : '- No existe'  }</td></tr>)
-			var tmp_ = this.partidas[i].map( 
-				(p,j) => <tr onClick={() => this.addRelation(p.iddetalle)} key={p.iddetalle} style={ p.style  } ><td>{p.index}</td><td>{p.descripcion}<i className={(p.className)?p.className:''}></i></td></tr>
-			)
-
-			partidas_rows.push(tmp_);
+			if(this.partidas[i]!==undefined){
+				partidas_rows.push(<tr key={i}><td colSpan="2">{i} { (this.partidas[i].length>0) ? '' : '- No existe'  }</td></tr>)
+				var tmp_ = this.partidas[i].map( 
+					(p,j) => <tr onClick={() => this.addRelation(p.iddetalle)} key={p.iddetalle} style={ p.style  } ><td>{p.index}</td><td>{p.descripcion}<i className={(p.className)?p.className:''}></i></td></tr>
+				)
+				partidas_rows.push(tmp_);
+			}
 		}
-		//this.setState({ _rows : partidas_rows});
 		return partidas_rows;
-		//return this.props.partidas.maps( (p) => <Partida iddetalle={p.iddetalle}  descripcion={p.descripcion} /> );		
 	}
 
 	render(){
@@ -33,10 +32,6 @@ class Partidas extends Component{
 	}
 }
 
-//<table className="table table-bordered"><thead><tr><th className="text-center" colSpan="2">Productos</th></tr></thead><tbody>{this.state._rows}</tbody></table>
 
-//<table className="table table-bordered"><thead><tr><th className="text-center" colSpan="2">Productos</th></tr></thead><tbody>{this.props.partidas}</tbody></table>
-
-//<table className="table table-bordered"><thead><tr><th className="text-center" colSpan="2">Productos</th></tr></thead><tbody>{this.state._rows}</tbody></table>
 
 export default Partidas;
